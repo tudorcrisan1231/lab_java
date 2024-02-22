c5a4185c0dbca97ab12a6d3c381497bc34acbbf9
tudorcrisan1231 <crisantudor79@yahoo.com>
2024-02-22 15:31:59 +0200

    test

--- src/main/java/ex6/Main.java
+++ src/main/java/ex6/Main.java
@@ -2,7 +2,7 @@ package ex6;
 
 public class Main {
 //     Să se scrie un program care generează aleatoriu un număr întreg cuprins între 0 și 20.
-//Programul va determina dacă numărul aparține șirului lui Fobonacci.
+//Programul va determina dacă numărul aparține șirului lui Fobonacci. u
     public static void main(String[] args) {
         int n = (int) (Math.random() * 20);
         System.out.println(n);
